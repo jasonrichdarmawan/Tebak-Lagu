6cf9eea0028279a39403ae535b05d3e57f24ac61
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-03-20 12:06:42 +0700

    Initial Commit

---
+++ Tebak Lagu/Tebak_LaguApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tebak_LaguApp.swift
+//  Tebak Lagu
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Tebak_LaguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
